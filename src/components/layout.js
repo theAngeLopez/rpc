46d5da961e272e49381ee087c2e84a10b5edba26
Angel Lopez <angel.lopez@thinkresearch.com>
2019-02-25 17:07:11 -0500

    Structure changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,8 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
 import "./main.scss"
-
+import "./layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -23,17 +22,14 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            margin: `0`,
             paddingTop: 0,
           }}
         >
           <main>{children}</main>
+
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            © {new Date().getFullYear()} Red Plumbing Co. Toronto
           </footer>
         </div>
       </>
